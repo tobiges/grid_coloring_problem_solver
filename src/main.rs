529edaca2c78c15643bdb2ab57f86d28bde857e6
Tobias Gessler <gessler.tobi@gmail.com>
2022-01-15 16:34:57 +0100

    working solver 16 is fast, 18 takes about 5 hours

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,25 @@
 use std::time::Instant;
-
-// use exercise_45_b::grid::Grid;
 use varisat::{Lit, Solver};
 
 fn main() {
-    solve(2);
     solve(8);
     solve(10);
-    solve(14);
-    solve(15);
     solve(16);
+
+
+    // 17x17 and 18x18 take about 5 hours each
+    // solve(17);
+    // solve(18);
+
+
+    // for i in 2..=16 {
+    //     solve(i);
+    // }
 }
 
 fn solve(size: usize) {
     let mut solver = Solver::new();
-
-    // let formula = build_formula(size);
-    // solver.add_formula(&formula);
-
-    let dimacs_cnf = build_dimacs_cnf(size);
+    let dimacs_cnf = build_dimacs_cnf(size + ((size % 2 != 0) as usize));
     solver.add_dimacs_cnf(&dimacs_cnf[..]).expect("parse error");
 
     println!("............................................");
@@ -44,163 +45,103 @@ fn solve(size: usize) {
 }
 
 fn build_dimacs_cnf(size: usize) -> Vec<u8> {
+    if size % 2 != 0 {
+        panic!("solve: size must be even!");
+    }
+
     let mut dimacs_cnf = Vec::new();
-
-    // let mut c_count = 0;
-    // for i in 0..size - 1 {
-    //     for j in i + 1..size {
-    //         for k in 0..size - 1 {
-    //             for l in k + 1..size {
-    //                 c_count += 4;
-    //             }
-    //         }
-    //     }
-    // }
-
-    // dimacs_cnf.extend_from_slice(format!("p cnf {} {}\n", 2 * size * size, c_count).as_bytes());
-
     for i in 0..size - 1 {
         for j in i + 1..size {
             for k in 0..size - 1 {
                 for l in k + 1..size {
-                    let (a_ik, b_ik) = get_dimacs_pos_id(size, i, k);
-                    let (a_il, b_il) = get_dimacs_pos_id(size, i, l);
-                    let (a_jk, b_jk) = get_dimacs_pos_id(size, j, k);
-                    let (a_jl, b_jl) = get_dimacs_pos_id(size, j, l);
+                    let ik = get_dimacs_pos_id(size, i, k);
+                    let il = get_dimacs_pos_id(size, i, l);
+                    let jk = get_dimacs_pos_id(size, j, k);
+                    let jl = get_dimacs_pos_id(size, j, l);
                     dimacs_cnf.extend_from_slice(
-                        format!(
-                            "{} {} {} {} {} {} {} {} 0\n",
-                            a_ik, b_ik, a_il, b_il, a_jk, b_jk, a_jl, b_jl
-                        )
-                        .as_bytes(),
-                    );
-                    dimacs_cnf.extend_from_slice(
-                        format!(
-                            "-{} {} -{} {} -{} {} -{} {} 0\n",
-                            a_ik, b_ik, a_il, b_il, a_jk, b_jk, a_jl, b_jl
-                        )
-                        .as_bytes(),
-                    );
-                    dimacs_cnf.extend_from_slice(
-                        format!(
-                            "{} -{} {} -{} {} -{} {} -{} 0\n",
-                            a_ik, b_ik, a_il, b_il, a_jk, b_jk, a_jl, b_jl
-                        )
-                        .as_bytes(),
-                    );
-                    dimacs_cnf.extend_from_slice(
-                        format!(
-                            "-{} -{} -{} -{} -{} -{} -{} -{} 0\n",
-                            a_ik, b_ik, a_il, b_il, a_jk, b_jk, a_jl, b_jl
-                        )
-                        .as_bytes(),
+                        format!("-{} -{} -{} -{} 0\n", ik, il, jk, jl).as_bytes(),
                     );
                 }
             }
+        }
+    }
+    for r in 0..size / 2 {
+        for c in 0..size / 2 {
+            let x1 = get_dimacs_pos_id(size, r, c);
+            let x2 = get_dimacs_pos_id(size, c, size - r - 1);
+            let x3 = get_dimacs_pos_id(size, size - r - 1, size - c - 1);
+            let x4 = get_dimacs_pos_id(size, size - c - 1, r);
+
+            // at least x must be 1
+            dimacs_cnf.extend_from_slice(format!("{} {} {} {} 0\n", x1, x2, x3, x4).as_bytes());
+
+            // at most one x can be one
+            dimacs_cnf.extend_from_slice(format!("-{} -{} 0\n", x1, x2).as_bytes());
+            dimacs_cnf.extend_from_slice(format!("-{} -{} 0\n", x1, x3).as_bytes());
+            dimacs_cnf.extend_from_slice(format!("-{} -{} 0\n", x1, x4).as_bytes());
+            dimacs_cnf.extend_from_slice(format!("-{} -{} 0\n", x2, x3).as_bytes());
+            dimacs_cnf.extend_from_slice(format!("-{} -{} 0\n", x2, x4).as_bytes());
+            dimacs_cnf.extend_from_slice(format!("-{} -{} 0\n", x3, x4).as_bytes());
         }
     }
 
     dimacs_cnf
 }
 
-fn get_dimacs_pos_id(size: usize, row: usize, col: usize) -> (usize, usize) {
-    let num = (row * size + col) * 2 + 1; // 1 indexed in dimacs
-    (num, num + 1)
+fn get_dimacs_pos_id(size: usize, row: usize, col: usize) -> usize {
+    row * size + col + 1
 }
 
-fn get_cords_from_lit(size: usize, lit: Lit) -> (usize, usize, bool) {
-    let mut pos = lit.index();
-    let is_a = pos % 2 == 0;
-    pos /= 2;
+fn get_cords_from_lit(mut size: usize, lit: Lit) -> (usize, usize) {
+    if size % 2 != 0 {
+        size += 1;
+    }
+    let pos = lit.index();
     let row = pos / size;
     let col = pos % size;
-    (row, col, is_a)
+    (row, col)
 }
 
 fn print_solution(size: usize, model: Vec<Lit>) {
-    let mut grid = vec![vec![0_u32; size]; size];
+    let mut grid = vec![vec![false; size]; size];
     for lit in model {
-        let (row, col, is_a) = get_cords_from_lit(size, lit);
-        if lit.is_positive() {
-            grid[row][col] |= 1 << (!is_a as u32);
+        let (row, col) = get_cords_from_lit(size, lit);
+        if row != size && col != size {
+            grid[row][col] = lit.is_positive();
         }
     }
-    for line in grid {
+    let mut solution = vec![vec![0_u32; size]; size];
+    for color in 1..=4 {
+        for r in 0..size {
+            for c in 0..size {
+                if grid[r][c] {
+                    solution[r][c] = color;
+                }
+            }
+        }
+        grid = rotate_90_degree(grid);
+    }
+
+    for line in solution {
         let line: String = line
             .iter()
-            .map(|&d| char::from_digit(d + 1, 10).unwrap())
+            .map(|&d| char::from_digit(d, 10).unwrap())
             .collect();
         println!("{}", line);
     }
 }
 
-// fn build_formula(size: usize) -> CnfFormula {
-//     let mut formula = CnfFormula::new();
+// rotates vec, must be quadratic
+fn rotate_90_degree(v: Vec<Vec<bool>>) -> Vec<Vec<bool>> {
+    let size = v.len();
+    let mut rotated = vec![vec![false; size]; size];
 
-//     let mut grid = Vec::with_capacity(size);
-//     let mut var_count = 0;
-//     for _ in 0..size {
-//         let mut grid_row = Vec::with_capacity(size);
-//         for _ in 0..size {
-//             grid_row.push((Var::from_index(var_count), Var::from_index(var_count + 1)));
-//             var_count += 2;
-//         }
-//         grid.push(grid_row);
-//     }
+    #[allow(clippy::needless_range_loop)]
+    for r in 0..size {
+        for c in 0..size {
+            rotated[r][c] = v[c][size - r - 1];
+        }
+    }
 
-//     for i in 0..size - 1 {
-//         for j in i + 1..size {
-//             for k in 0..size - 1 {
-//                 for l in k + 1..size {
-//                     let (a_ik, b_ik) = grid[i][k];
-//                     let (a_il, b_il) = grid[i][l];
-//                     let (a_jk, b_jk) = grid[j][k];
-//                     let (a_jl, b_jl) = grid[i][l];
-//                     // let x =
-//                     formula.add_clause(&[
-//                         a_ik.positive(),
-//                         b_ik.positive(),
-//                         a_il.positive(),
-//                         b_il.positive(),
-//                         a_jk.positive(),
-//                         b_jk.positive(),
-//                         a_jl.positive(),
-//                         b_jl.positive(),
-//                     ]);
-//                     formula.add_clause(&[
-//                         a_ik.negative(),
-//                         b_ik.positive(),
-//                         a_il.negative(),
-//                         b_il.positive(),
-//                         a_jk.negative(),
-//                         b_jk.positive(),
-//                         a_jl.negative(),
-//                         b_jl.positive(),
-//                     ]);
-//                     formula.add_clause(&[
-//                         a_ik.positive(),
-//                         b_ik.negative(),
-//                         a_il.positive(),
-//                         b_il.negative(),
-//                         a_jk.positive(),
-//                         b_jk.negative(),
-//                         a_jl.positive(),
-//                         b_jl.negative(),
-//                     ]);
-//                     formula.add_clause(&[
-//                         a_ik.negative(),
-//                         b_ik.negative(),
-//                         a_il.negative(),
-//                         b_il.negative(),
-//                         a_jk.negative(),
-//                         b_jk.negative(),
-//                         a_jl.negative(),
-//                         b_jl.negative(),
-//                     ]);
-//                 }
-//             }
-//         }
-//     }
-
-//     formula
-// }
+    rotated
+}
